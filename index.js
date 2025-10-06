e453f611533b68a262a09358a01dd2d0d1012c78
Tinku112 <tinkukool.ts@gmail.com>
2025-10-07 01:52:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const hbs = require("hbs")
 const path = require("path")
 
-const Router = require("./routes/Router")
+const Router = require("../routes/Router")
 const app = express()
 
 app.set("view engine", "hbs")
